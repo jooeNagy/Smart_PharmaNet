b67f4a50603e2ceca66e7644b9d1830f327e4dbd
jooeNagy <ynagy0424@gmail.com>
2025-02-17 15:26:23 +0200

    jwt authentication system

--- smartPharmacy/accounts/urls.py
+++ smartPharmacy/accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import OwnerRegisterView, PharmacyCreateView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from .views import OwnerRegisterView, PharmacyCreateView, EmailTokenObtainPairView, CustomLogoutView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenBlacklistView
 
 urlpatterns = [
     path('register/', OwnerRegisterView.as_view(), name='owner-registeration'),
     path('pharmacies/', PharmacyCreateView.as_view(), name='pharmacy-create'),
-    path('token/', TokenObtainPairView.as_view(), name='login'),
-    path('token-refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('token/', EmailTokenObtainPairView.as_view(), name='token-obtain-pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('logout/', CustomLogoutView.as_view(), name='token-blacklist'),
 ]
