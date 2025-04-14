cc359602473d4409af8bd8369fb80fb36038b8c5
Youssef Nagy <ynagy0424@gmail.com>
2025-04-14 17:14:05 +0200

    make migrations for location new fields

--- accounts/models.py
+++ accounts/models.py
@@ -24,9 +24,9 @@ class Owner(models.Model):
 class Pharmacy(models.Model):
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE, related_name='pharmacies')
     name = models.CharField(max_length=100)
-    city = models.CharField(max_length=50)
-    latitude = models.DecimalField(max_digits=9, decimal_places=6)
-    longitude = models.DecimalField(max_digits=9, decimal_places=6)
+    city = models.CharField(max_length=50, blank=True, null=True)
+    latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
+    longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     license_number = models.CharField(max_length=50)
     password = models.CharField(max_length=128, null=True, blank=True)
 
