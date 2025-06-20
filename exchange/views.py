5805776e425ef6ded4f0deb06a72c6aac00296f0
jooeNagy <ynagy0424@gmail.com>
2025-06-20 20:58:40 +0300

    delete env directory

--- exchange/views.py
+++ exchange/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 
-from django.shortcuts import get_object_or_404  # ✅ Import added
+from django.shortcuts import get_object_or_404  
 from rest_framework.response import Response
 from rest_framework.exceptions import PermissionDenied
 
