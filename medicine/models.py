fbdfe072ca8782d2f1307152f37c61179d562228
jooeNagy <ynagy0424@gmail.com>
2025-06-22 12:37:03 +0300

    update medicine endpoint

--- medicine/models.py
+++ medicine/models.py
@@ -24,8 +24,6 @@ class Medicine(models.Model):
     quantity_to_sell = models.IntegerField(null=True, blank=True)  # Fixed: Added blank=True
     price_sell = models.DecimalField(max_digits=8, decimal_places=2, blank=False, null=False, default=0.00)
     
-    # Buy = models.BooleanField(null=True, default=False)
-    # quantity_to_Buy = models.IntegerField(null=True, blank=False)  # Fixed: Added blank=True
     
     
     def __str__(self):
@@ -49,7 +47,7 @@ class Medicine(models.Model):
             self.quantity_to_sell = None  # Explicitly set to None (allowed with blank=True)
 
     def save(self, *args, **kwargs):
-        from exchange.models import ExchangeMedciene, Order_exchange  # Import here to avoid circular import
+        from exchange.models import ExchangeMedciene  # Import here to avoid circular import
 
         self.full_clean()  # Validate fields
         super().save(*args, **kwargs)  # Save FIRST to ensure ID exists
@@ -73,29 +71,6 @@ class Medicine(models.Model):
                     "quantity_to_sell": "Cannot exceed available quantity"
                 })
             
-        
-        # if not self.Buy:
-        #     ExchangeMedciene.objects.filter(
-        #         medicine=self,
-        #         operation=ExchangeMedciene.Status.BUY
-        #     ).delete()
-        # else:
-        #     if self.quantity_to_Buy <= self.quantity_to_sell:
-        #         self.quantity -= self.quantity_to_Buy
-        #         self.quantity_to_sell -= self.quantity_to_Buy
-        #         self.save()
-        #         Order_exchange.objects.update_or_create(
-        #             medicine=self,
-        #             # pharmacy_buyer='',
-        #             defaults={
-        #                 'quantity': self.quantity_to_Buy,
-        #                 'status': 'Pending',
-        #             }
-        #         )
-        #     else:
-        #         raise serializers.ValidationError({
-        #             "quantity_to_buy": "Cannot exceed available quantity to sell"
-        #         })
             
 
 
